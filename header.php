e0a127078f79a3b94eec3089634e916d1d2c9c39
Isaac Maldonado <isaacmaldonado21@gmail.com>
2025-08-31 22:19:10 -0400

    proyecto wordpress

--- header.php
+++ header.php
@@ -8,3 +8,44 @@
  </head>
    <body>
 
+        <header class="encabezado-sitio">
+            <div class="contenedor">
+                <div class="logo">
+                    <a href="<?php echo esc_url (home_url('/') ); ?>">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="">
+                    </a>
+                </div> <!-- .logo-->
+            </div><!-- .contenedor -->
+        </header>
+
+        <div class="informacion-encabezado">
+            <div class="redes-sociales">
+                <?php 
+                $args = array(
+                    'theme_location' => 'social-menu',
+                    'container' => 'nav',
+                    'container_class' => 'menu-social',
+                    'container_id' => 'menu-social',
+                    'link_before' => '<span class="sr-text">',
+                    'link_after' => '</span>'
+                );
+
+                wp_nav_menu($args);
+                
+                ?>
+            </div>
+        </div>
+
+        <nav class="menu-sitio">
+            <div class="contenedor navegacion">
+                <?php 
+                $args = array(
+                    'theme_location' => 'header-menu',
+                    'container' => 'nav',
+                    'container_class' => 'menu-sitio'
+                ); 
+
+                    wp_nav_menu($args);
+                ?>
+            </div>
+        </nav>
