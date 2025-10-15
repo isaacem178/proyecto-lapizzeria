04660159fb0e027cfcc40bd704e79bbbed92d7ec
Isaac Maldonado <isaacmaldonado21@gmail.com>
2025-10-15 06:02:45 -0400

    functions, index, page-nuestras-especialidades, style.css

--- index.php
+++ index.php
@@ -1,5 +1,36 @@
 <?php get_header(); ?>
+    
+<?php 
 
-<h1>Hola desde index.php</h1>
+    $pagina_blog = get_option('page_for_posts');
+    $imagen = get_post_thumbnail_id($pagina_blog);
+    $imagen = wp_get_attachment_image_src($imagen, 'full');
+
+?>
+
+    <pre>
+        <?php var_dump($imagen); ?> 
+    </pre>
+
+    <div class="hero" style="background-image:url(<?php echo $imagen[0]; ?>)">
+            <div class="contenido-hero">
+                <div class="texto-hero">
+                    <h1><?php echo get_the_title($pagina_blog); ?></h1>
+                 
+                </div>
+            </div>
+        </div>
+
+
+    <div class="principal contenedor">
+        <main class="texto-centrado contenido-paginas">
+            <?php while(have_posts()): the_post(); ?>
+                <article class="entrada-blog">
+                    <?php the_title(); ?> 
+                </article>
+            <?php endwhile; ?>
+        </main>
+    </div>
 
 <?php get_footer(); ?>
+
