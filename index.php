29b8d3fec29113cf2e5c5969c46749b66d035728
Isaac Maldonado <isaacmaldonado21@gmail.com>
2025-11-08 08:43:40 -0400

    added note to isaac.txt

--- index.php
+++ index.php
@@ -1,21 +1,11 @@
 <?php get_header(); ?>
 
-<?php 
 
-$pagina_blog = get_option('page_for_posts');
-$imagen = get_post_thumbnail_id($pagina_blog);
-$imagen = wp_get_attachment_image_src($imagen, 'full');
-
-?>
-<pre>
-    <?php var_dump($imagen); ?> 
-</pre>
 
 <div class="hero" style="background-image:url(<?php echo $imagen[0]; ?>)">
     <div class="contenido-hero">
         <div class="texto-hero">
             <h1><?php echo get_the_title($pagina_blog); ?></h1>
-
         </div>
     </div>
 </div>
@@ -43,7 +33,9 @@ $imagen = wp_get_attachment_image_src($imagen, 'full');
                             <?php the_author(); ?>
                         </p>
                     </div>
-                    <?php the_tags(); ?>
+
+                    <!--  <?php the_tags(); ?>  -->
+
                 </header>
                 <div class="contenido-entrada">
                     <?php the_excerpt(); ?>  
